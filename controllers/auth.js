d200e8021e00b1c274bc34a36340408a9ca250e9
Ilya Atamas <atamas1llya@gmail.com>
2017-12-16 17:22:21 +0200

    Add different auth modes support

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,12 +29,14 @@ export const registerLocal = async (req, res, next) => {
     .json({
       message: 'You\'re successfully registered!',
       token,
+      profile: user,
     });
 };
 
 
 export const handleLoginSuccess = async (req, res, next) => {
   const { user } = req;
+  const { mode } = req.body;
   let token;
 
   try {
@@ -46,7 +48,16 @@ export const handleLoginSuccess = async (req, res, next) => {
     });
   }
 
-  res
-    .status(200)
-    .redirect(`http://localhost:8080/login/success?auth_token=${token}`);
+  if (mode === 'inline') {
+    res
+      .status(200)
+      .json({
+        token,
+        profile: user,
+      })
+  } else {
+    res
+      .status(200)
+      .redirect(`http://localhost:8080/login/success?auth_token=${token}`);
+  }
 };
